2723e7c67c400f3e04e2c0ec9db365e736ab396e
ramonhoyo <ramonandreshoyodiaz@gmail.com>
2018-01-28 14:32:32 -0400

    some changes

--- ejemplo_Dynamic_cast.cpp
+++ ejemplo_Dynamic_cast.cpp
@@ -1,67 +1,65 @@
 #include <iostream>
 using namespace std;
 
-	class A{
-		public:
+class A{
+	public:
 		int a;
-		virtual void imprimir() {cout<<"yo soy A\n";};
-	};
+		virtual void print() { cout<<"I'm A\n"; };
+};
 	
-	class B:virtual public A{
-		public:
+class B: virtual public A{
+	public:
 		int b;
-		void imprimir(){cout<<"yo soy B\n";};
-	};
+		void print(){cout<<"I'm B\n";};
+};
 
-	class C:virtual public A{
-		public:
-			int c;
-		void imprimir(){cout<<"yo soy C\n";}
-	};
+class C: virtual public A {
+	public:
+		int c;
+		void print(){cout<<"yo soy C\n";}
+};
 
-	class D:public C, public B{
-		public:
-			int d;
-		void imprimir(){cout<<"yo soy D\n";}
-	};
+class D: public C, public B{
+	public:
+		int d;
+		void print(){cout<<"yo soy D\n";}
+};
 
 
-	int main(){
+int main(){
 	A *abc[4];
-		abc[0]= new A;
-		abc[1]= new B;
-		abc[2]= new C;
-		abc[3]=new D;
-		
-		//necesitamos apuntadores auxiliares al el tipo que deseamos castear.
-	B*	B_aux;
+	abc[0]= new A;
+	abc[1]= new B;
+	abc[2]= new C;
+	abc[3]=new D;
+	
 	C* 	C_aux;
 	D* D_aux;
 
-		//hacemos los casteos.
-	B_aux=dynamic_cast<B*>(abc[1]);//B_aux tiene acceso a todos los metodos publicos de B. 	
-	C_aux=dynamic_cast<C*>(abc[2]);//C_aux tiene acceso a todos los metodos publicos de C. 	
-	D_aux=dynamic_cast<D*>(abc[3]);//D_aux tiene acceso a todos los metodos publicos de D.	
-		abc[0]->a=5;
-		B_aux->b=10;
-		C_aux->c=15;
-		D_aux->d=20;
+	B* B_aux = dynamic_cast<B*>(abc[1]);//B_aux has access to full members of B class. 	
+	C* C_aux = dynamic_cast<C*>(abc[2]);//C_aux has access to full members of C class. 	
+	D* D_aux = dynamic_cast<D*>(abc[3]);//D_aux has access to full members of B class.	
+	
+	abc[0]->a=5;
+	B_aux->b=10;
+	C_aux->c=15;
+	D_aux->d=20;
 		
-		cout<<abc[0]->a<<"\n";
-		cout<<B_aux->b<<"\n";
-		cout<<C_aux->c<<"\n";
-		cout<<D_aux->d<<"\n";
+	cout<<abc[0]->a<<"\n";
+	cout<<B_aux->b<<"\n";
+	cout<<C_aux->c<<"\n";
+	cout<<D_aux->d<<"\n";
 		
 
-		D_aux->a=50;
-		cout<<D_aux->a<<"\n";
+	D_aux->a=50;
+	cout<<D_aux->a<<"\n";
 
-		abc[0]->imprimir();
-		abc[1]->imprimir();
-		abc[2]->imprimir();
-		abc[3]->imprimir();
+	abc[0]->print();
+	abc[1]->print();
+	abc[2]->print();
+	abc[3]->print();
 
-		return 0;
-	}
+	return 0;
+}
 
 	
